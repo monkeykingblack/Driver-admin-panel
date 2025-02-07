b4f2334ae5fccb6c15667384d31dfe5214b25704
Trung Dang Quang <d.q.t.7775@gmail.com>
2025-02-07 16:22:46 +0700

    init project

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+//@ts-ignore
+import tailgrids from "tailgrids/plugin";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [tailgrids],
+} satisfies Config;
