369bd42c7207860df5ddddfdb4871c940adc7db4
Trung Dang Quang <d.q.t.7775@gmail.com>
2025-02-07 17:34:54 +0700

    setup dev tools

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,49 @@
-import type { Config } from "tailwindcss";
-//@ts-ignore
-import tailgrids from "tailgrids/plugin";
+import type { Config } from 'tailwindcss';
+
+// @ts-expect-error no need to define type here
+import tailgrids from 'tailgrids/plugin';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    darkMode: 'class',
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
+    screens: {
+      xs: '400px',
+      // => @media (min-width: 400px) { ... }
+
+      sm: '540px',
+      // => @media (min-width: 540px) { ... }
+
+      md: '720px',
+      // => @media (min-width: 720px) { ... }
+
+      lg: '960px',
+      // => @media (min-width: 960px) { ... }
+
+      xl: '1140px',
+      // => @media (min-width: 1240px) { ... }
+
+      '2xl': '1320px',
+      // => @media (min-width: 1320px) { ... }
+    },
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
+
   plugins: [tailgrids],
 } satisfies Config;
